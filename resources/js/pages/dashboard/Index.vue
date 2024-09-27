e18d9e38e96fdac36d7c7554760397d74ab3eb61
krz <bazzkairo@gmail.com>
2024-09-27 10:52:52 +0700

    DONE

--- resources/js/pages/dashboard/Index.vue
+++ resources/js/pages/dashboard/Index.vue
@@ -1,128 +1,209 @@
 <template>
-  <!--begin::Mixed Widget 14-->
-  <div
-    :class="widgetClasses"
-    class="card theme-dark-bg-body"
-    :style="`background-color: ${widgetColor}`"
-  >
+  <!--begin::Mixed Widget 10-->
+  <div :class="widgetClasses" class="card">
     <!--begin::Body-->
-    <div class="card-body d-flex flex-column">
-      <!--begin::Wrapper-->
-      <div class="d-flex flex-column mb-7">
-        <!--begin::Title-->
-        <a href="#" class="text-gray-900 text-hover-primary fw-bold fs-3"
-          >Summary</a
-        >
-        <!--end::Title-->
+    <div
+      class="card-body p-0 d-flex justify-content-between flex-column overflow-hidden"
+    >
+      <!--begin::Hidden-->
+      <div class="d-flex flex-stack flex-wrap flex-grow-1 px-9 pt-9 pb-3">
+        <div class="me-2">
+          <span class="fw-bold text-gray-800 d-block fs-3">Penjualan Tiket</span>
+
+          <span class="text-gray-500 fw-semibold">Oct 8 - Oct 26 21</span>
+        </div>
+
+        <div class="fw-bold fs-3" :class="`text-${chartColor}`">Rp.150.000.000</div>
       </div>
-      <!--end::Wrapper-->
+      <!--end::Hidden-->
 
-      <!--begin::Row-->
-      <div class="row g-0">
-        <!--begin::Col-->
-        <div class="col-6">
-          <div class="d-flex align-items-center mb-9 me-2">
-            <!--begin::Symbol-->
-            <div class="symbol symbol-40px me-3">
-              <div class="symbol-label bg-body bg-opacity-50">
-                <KTIcon icon-name="abstract-42" icon-class="fs-1 text-gray-900" />
-              </div>
-            </div>
-            <!--end::Symbol-->
-
-            <!--begin::Title-->
-            <div>
-              <div class="fs-5 text-dark fw-bold lh-1">$50K</div>
-              <div class="fs-7 text-gray-600 fw-semibold">Sales</div>
-            </div>
-            <!--end::Title-->
-          </div>
-        </div>
-        <!--end::Col-->
-
-        <!--begin::Col-->
-        <div class="col-6">
-          <div class="d-flex align-items-center mb-9 ms-2">
-            <!--begin::Symbol-->
-            <div class="symbol symbol-40px me-3">
-              <div class="symbol-label bg-body bg-opacity-50">
-                <KTIcon icon-name="abstract-45" icon-class="fs-1 text-gray-900" />
-              </div>
-            </div>
-            <!--end::Symbol-->
-
-            <!--begin::Title-->
-            <div>
-              <div class="fs-5 text-dark fw-bold lh-1">$4,5K</div>
-              <div class="fs-7 text-gray-600 fw-semibold">Revenue</div>
-            </div>
-            <!--end::Title-->
-          </div>
-        </div>
-        <!--end::Col-->
-
-        <!--begin::Col-->
-        <div class="col-6">
-          <div class="d-flex align-items-center me-2">
-            <!--begin::Symbol-->
-            <div class="symbol symbol-40px me-3">
-              <div class="symbol-label bg-body bg-opacity-50">
-                <KTIcon icon-name="abstract-21" icon-class="fs-1 text-gray-900" />
-              </div>
-            </div>
-            <!--end::Symbol-->
-
-            <!--begin::Title-->
-            <div>
-              <div class="fs-5 text-dark fw-bold lh-1">40</div>
-              <div class="fs-7 text-gray-600 fw-semibold">Tasks</div>
-            </div>
-            <!--end::Title-->
-          </div>
-        </div>
-        <!--end::Col-->
-
-        <!--begin::Col-->
-        <div class="col-6">
-          <div class="d-flex align-items-center ms-2">
-            <!--begin::Symbol-->
-            <div class="symbol symbol-40px me-3">
-              <div class="symbol-label bg-body bg-opacity-50">
-                <KTIcon icon-name="abstract-44" icon-class="fs-1 text-gray-900" />
-              </div>
-            </div>
-            <!--end::Symbol-->
-
-            <!--begin::Title-->
-            <div>
-              <div class="fs-5 text-dark fw-bold lh-1">$5.8M</div>
-              <div class="fs-7 text-gray-600 fw-semibold">Sales</div>
-            </div>
-            <!--end::Title-->
-          </div>
-        </div>
-        <!--end::Col-->
-      </div>
-      <!--end::Row-->
+      <!--begin::Chart-->
+      <apexchart
+        ref="chartRef"
+        class="mixed-widget-10-chart"
+        :options="chart"
+        :series="series"
+        type="bar"
+        :height="chartHeight"
+      ></apexchart>
+      <!--end::Chart-->
     </div>
   </div>
-  <!--end::Mixed Widget 14-->
+  <!--end::Mixed Widget 10-->
 </template>
 
 <script lang="ts">
 import { getAssetPath } from "@/core/helpers/assets";
-import { defineComponent } from "vue";
+import { computed, defineComponent, onBeforeMount, ref, watch } from "vue";
+import { getCSSVariableValue } from "@/assets/ts/_utils";
+import type VueApexCharts from "vue3-apexcharts";
+import type { ApexOptions } from "apexcharts";
+import { useThemeStore } from "@/stores/theme";
 
 export default defineComponent({
-  name: "widget-14",
+  name: "widget-12",
   props: {
     widgetClasses: String,
-    widgetColor: String,
+    chartColor: String,
+    chartHeight: String,
   },
-  setup() {
+  setup(props) {
+    const chartRef = ref<typeof VueApexCharts | null>(null);
+    const chart = ref<ApexOptions>({});
+    const store = useThemeStore();
+
+    const series = ref([
+      {
+        name: "Net Profit",
+        data: [50, 60, 70, 80, 60, 50, 70, 60],
+      },
+      {
+        name: "Revenue",
+        data: [50, 60, 70, 80, 60, 50, 70, 60],
+      },
+    ]);
+
+    const themeMode = computed(() => {
+      return store.mode;
+    });
+
+    onBeforeMount(() => {
+      Object.assign(
+        chart.value,
+        chartOptions(props.chartColor, props.chartHeight)
+      );
+    });
+
+    const refreshChart = () => {
+      if (!chartRef.value) {
+        return;
+      }
+
+      chartRef.value.updateOptions(
+        chartOptions(props.chartColor, props.chartHeight)
+      );
+    };
+
+    watch(themeMode, () => {
+      refreshChart();
+    });
+
     return {
+      chart,
+      series,
+      chartRef,
+      refreshChart,
       getAssetPath,
     };
   },
 });
+
+const chartOptions = (
+  color: string = "primary",
+  chartHeight: string = "auto"
+): ApexOptions => {
+  const labelColor = getCSSVariableValue("--bs-gray-500");
+  const borderColor = getCSSVariableValue("--bs-gray-200");
+  const secondaryColor = getCSSVariableValue("--bs-gray-300");
+  const baseColor = getCSSVariableValue(`--bs-${color}`);
+
+  return {
+    chart: {
+      fontFamily: "inherit",
+      type: "bar",
+      height: chartHeight,
+      toolbar: {
+        show: false,
+      },
+    },
+    plotOptions: {
+      bar: {
+        horizontal: false,
+        columnWidth: "50%",
+        borderRadius: 5,
+      },
+    },
+    legend: {
+      show: false,
+    },
+    dataLabels: {
+      enabled: false,
+    },
+    stroke: {
+      show: true,
+      width: 2,
+      colors: ["transparent"],
+    },
+    xaxis: {
+      categories: ["Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep"],
+      axisBorder: {
+        show: false,
+      },
+      axisTicks: {
+        show: false,
+      },
+      labels: {
+        style: {
+          colors: labelColor,
+          fontSize: "12px",
+        },
+      },
+    },
+    yaxis: {
+      labels: {
+        style: {
+          colors: labelColor,
+          fontSize: "12px",
+        },
+      },
+    },
+    fill: {
+      type: "solid",
+    },
+    states: {
+      normal: {
+        filter: {
+          type: "none",
+          value: 0,
+        },
+      },
+      hover: {
+        filter: {
+          type: "none",
+          value: 0,
+        },
+      },
+      active: {
+        allowMultipleDataPointsSelection: false,
+        filter: {
+          type: "none",
+          value: 0,
+        },
+      },
+    },
+    tooltip: {
+      style: {
+        fontSize: "12px",
+      },
+      y: {
+        formatter: function (val) {
+          return "$" + val + " revenue";
+        },
+      },
+    },
+    colors: [baseColor, secondaryColor],
+    grid: {
+      padding: {
+        top: 10,
+      },
+      borderColor: borderColor,
+      strokeDashArray: 4,
+      yaxis: {
+        lines: {
+          show: true,
+        },
+      },
+    },
+  };
+};
 </script>
